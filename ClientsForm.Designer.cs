06526370ffabf824e36c8d44926137bdf750792f
VikaShchegol <134399555+VikaShchegol@users.noreply.github.com>
2024-06-02 20:45:20 +0300

    Add files via upload

---
+++ ClientsForm.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace course
+{
+    partial class ClientsForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.DataGridView dataGridViewClients;
+        private System.Windows.Forms.Button buttonBack;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.dataGridViewClients = new System.Windows.Forms.DataGridView();
+            this.buttonBack = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewClients)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewClients
+            // 
+            this.dataGridViewClients.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridViewClients.BackgroundColor = System.Drawing.Color.LavenderBlush;
+            this.dataGridViewClients.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewClients.Location = new System.Drawing.Point(18, 18);
+            this.dataGridViewClients.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridViewClients.Name = "dataGridViewClients";
+            this.dataGridViewClients.Size = new System.Drawing.Size(410, 272);
+            this.dataGridViewClients.TabIndex = 0;
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.BackColor = System.Drawing.Color.LightCyan;
+            this.buttonBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.buttonBack.Location = new System.Drawing.Point(113, 304);
+            this.buttonBack.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(211, 47);
+            this.buttonBack.TabIndex = 1;
+            this.buttonBack.Text = "Назад";
+            this.buttonBack.UseVisualStyleBackColor = false;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // ClientsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Linen;
+            this.ClientSize = new System.Drawing.Size(446, 365);
+            this.Controls.Add(this.buttonBack);
+            this.Controls.Add(this.dataGridViewClients);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "ClientsForm";
+            this.Text = "Дані клієнтів";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewClients)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ ClientsForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace course
+{
+    public partial class ClientsForm : Form
+    {
+        private MySqlConnection conn;
+
+        public ClientsForm(MySqlConnection connection)
+        {
+            InitializeComponent();
+            conn = connection;
+            LoadClients();
+        }
+
+        private void LoadClients()
+        {
+            string query = "SELECT Ім_я, Прізвище, Електронна_пошта FROM Clients";
+
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                dataGridViewClients.DataSource = dataTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка завантаження клієнтів: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
